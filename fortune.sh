55ca5fe50fc848692544ba99d2716511b2cddbf4
Eyitayo Itunu Babatope <112722909+eyitayoit-alt@users.noreply.github.com>
2026-07-18 03:30:03 -0400

    test

--- fortune.sh
+++ fortune.sh
@@ -5,8 +5,10 @@ echo -e "\n~~ Fortune Teller ~~\n"
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it"  "Ask again later")
 N=$(( $RANDOM % 6 ))
 
-echo ${RESPONSES[$N]}
+echo -e "\n${RESPONSES[$N]}"
+
 GET_FORTUNE(){
+  
 if [[ ! $1 ]]
 then
 echo Ask a yes or no question:
@@ -15,6 +17,7 @@ echo Try again. Make sure it ends with a question mark:
 fi
 read QUESTION
 }
+
 GET_FORTUNE
 until [[ $QUESTION =~ \?$ ]]
 do
