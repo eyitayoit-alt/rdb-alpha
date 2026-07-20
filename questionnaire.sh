a3904a6323b8710a42675c7cd73939a09d8a78ec
Eyitayo Itunu Babatope <112722909+eyitayoit-alt@users.noreply.github.com>
2026-07-20 01:19:33 -0400

    Save progress: 1490.1

--- questionnaire.sh
+++ questionnaire.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+
 echo -e "\n~~ Questionnaire ~~\n"
+
 QUESTION1="What's your name?"
+QUESTION2="Where are you from?"
+QUESTION3="What's your favorite coding website?"
+
 echo $QUESTION1
 read NAME
 
-
-QUESTION2="Where are you from?"
 echo $QUESTION2
 read LOCATION
 
-
-QUESTION3="What's your favorite coding website?"
 echo $QUESTION3
 read WEBSITE
 
